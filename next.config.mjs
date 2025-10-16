89a7be5e847e25f8e49d1ce41770afee64ddef70
Vitalii Y. <jakimoff1990@gmail.com>
2025-10-15 20:04:33 -0400

    fix: vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin("./src/app/localization/i18n.ts")
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: "export",
   distDir: "dist",
   images: {
     remotePatterns: [
